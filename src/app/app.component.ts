a4e8f713c71cb0539ddb40c37f94f2cb54006fd9
Lucas <lucasbrodrigues14@gmail.com>
2025-02-11 07:10:38 +0000

    can't setup tailwind with primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,37 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import Lara from "@primeng/themes/lara";
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterOutlet } from "@angular/router";
 import { invoke } from "@tauri-apps/api/core";
+import { ButtonModule } from "primeng/button";
+import { PrimeNG } from "primeng/config";
 
 @Component({
-    selector: 'app-root',
-    imports: [CommonModule, RouterOutlet],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+    selector: "app-root",
+    imports: [CommonModule, RouterOutlet, ButtonModule],
+    templateUrl: "./app.component.html",
+    styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  greetingMessage = "";
+    constructor(private primeng: PrimeNG) {
+        this.primeng.theme.set({
+            preset: Lara,
+            options: {
+                cssLayer: {
+                    name: "primeng",
+                    order: "tailwind-utilities, primeng",
+                },
+            },
+        });
+    }
+    greetingMessage = "";
 
-  greet(event: SubmitEvent, name: string): void {
-    event.preventDefault();
+    greet(event: SubmitEvent, name: string): void {
+        event.preventDefault();
 
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    invoke<string>("greet", { name }).then((text) => {
-      this.greetingMessage = text;
-    });
-  }
+        // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+        invoke<string>("greet", { name }).then((text) => {
+            this.greetingMessage = text;
+        });
+    }
 }
